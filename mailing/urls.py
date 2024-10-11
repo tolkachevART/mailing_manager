0c8ffd7e0083852c7b6f799efae4fb157dbcc988
tolkachevART <168184655+tolkachevART@users.noreply.github.com>
2024-10-11 06:10:37 +0300

    init -> homepage

---
+++ mailing/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+
+from mailing.apps import MailingConfig
+from mailing.views import home
+
+app_name = MailingConfig.name
+
+urlpatterns = [
+
+    path("", home, name='home'),
+]
